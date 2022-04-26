79839ddb958962c6d2b657837fc59555bac3daee
Christopher Nance <ctnance0@gmail.com>
2022-04-26 14:35:46 -0400

    added twitter contact

--- src/data/socialData.js
+++ src/data/socialData.js
@@ -1,5 +1,6 @@
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default [
@@ -12,6 +13,11 @@ export default [
     aria_label: "Github",
     href: "https://github.com/ctnance",
     icon: <GitHubIcon />,
+  },
+  {
+    aria_label: "Twitter",
+    href: "https://twitter.com/ctnance0",
+    icon: <TwitterIcon />,
   },
   {
     aria_label: "Linkedin",
